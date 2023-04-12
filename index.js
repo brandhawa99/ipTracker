520024f439be83214f825f0ba5a6210ef6ef2480
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-04-11 23:52:11 -0700

    asdf

---
+++ index.js
@@ -0,0 +1,14 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/sendValues/:id', function (req, res) {
+  const val = req.params
+  console.log(val);
+  res.send("hello")
+})
+
+app.listen(3002)
